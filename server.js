7286b2a3985df1f19fa9466e2179bc28f6b89e8d
DEVANSH <devanshk1661@gmail.com>
2026-02-24 13:37:01 +0530

    Add dynamic PORT support for deployment

--- server.js
+++ server.js
@@ -1654,14 +1654,17 @@ app.use((err, req, res, next) => {
 /* =====================================================
    SERVER START
 ===================================================== */
-const server = app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 // ================= PROCESS CRASH HANDLERS =================
 
-process.on("unhandledRejection", (reason, promise) => {
+process.on("unhandledRejection", (reason) => {
   console.error("UNHANDLED REJECTION:", reason);
+  process.exit(1);
 });
 
 process.on("uncaughtException", (err) => {
